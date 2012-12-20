b2eb9a17a3b8976e20da2152b45f3443cdce905c
YuviPanda <yuvipanda@gmail.com>
2012-12-20 17:03:53 +0530

    Number of requests much reduced now

--- mwuppet.py
+++ mwuppet.py
@@ -2,26 +2,37 @@
 import os
 import argparse
 from mwapi import MWApi
-from yaml import load
+from yaml import load, dump
 import time
 import re
 
 MODE_REGEX = re.compile(r'page:\s?(\S*)', re.I)
 
 api = MWApi('http://en.wikipedia.org')
+tokens = None
 
-def get_user_config():
-    config_path = os.path.expanduser("~/.mwuppet")
-    if os.path.exists(config_path):
-        config = load(open(config_path).read())
-    else:
-        config = {
-            "user": {
-                "username": raw_input("Enter your user name: "),
-                "password": raw_input("Enter your password: ")
-            }
-        }
-    return config
+def ensure_logged_in():
+    global tokens
+    cookies_path = os.path.expanduser("~/.mwuppet")
+    if os.path.exists(cookies_path):
+        data = load(open(cookies_path).read())
+        cookies = data['cookies']
+        tokens = data['tokens']
+        api.set_auth_cookie(cookies)
+        if api.validate_login():
+            return
+
+    # If it reaches here, that means we don't have a valid login
+    username = raw_input("Enter your username: ")
+    password = raw_input("Enter your password: ")
+    api.login(username, password)
+    
+    cookies = api.get_auth_cookie()
+    tokens = api.get_tokens()
+    open(cookies_path, 'w').write(dump({
+        'tokens': tokens,
+        'cookies': cookies
+    }))
 
 def parse_line(line):
     match = MODE_REGEX.search(line)
@@ -32,17 +43,9 @@ def parse_line(line):
 
 def save_page(page, text, summary):
     if not api.is_authenticated:
-        config = get_user_config()
-        api.login(config["user"]["username"], config["user"]["password"])
-        api.populateTokens()
+        ensure_logged_in()
 
-    print api.post({
-        "action": "edit",
-        "title": page,
-        "text": text,
-        "summary": summary,
-        "token": api.tokens["edittoken"]
-        })
+    print api.post(action="edit", title=page, text=text, summary=summary, token=tokens['edittoken'])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Sync code files with a Mediawiki installation")
@@ -57,4 +60,3 @@ if __name__ == "__main__":
         page = parse_line(firstline)
         if(page):
             save_page(page, f.read(), args.message)
-
