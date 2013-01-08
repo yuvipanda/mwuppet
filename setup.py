f617e7c3486945ab4aca91f2dfc75a30339ff816
Sibi <sibi@psibi.in>
2013-01-08 21:17:18 +0530

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+setup(
+        name="python-mwuppet",
+        version="0.0.1",
+        author="Yuvi Panda",
+        author_email="yuvipanda@gmail.com",
+        url="https://github.com/yuvipanda/mwuppet",
+        packages=['mwuppet',],
+        license="MIT License",
+        description = " Mediawiki UserScript Deploy Script",
+        long_description = open("README").read(),
+        install_requires = ["mwapi"]
+)
